d02cec384b16458bf7e43e9b34ed6dc8e28d3bfc
Harry-KNIGHT <eliott.knight@icloud.com>
2023-09-03 11:04:35 +0200

    doc: add documentation on mocks and mapper extension

--- Domain/Sources/Domain/SellerMapper.swift
+++ Domain/Sources/Domain/SellerMapper.swift
@@ -1,6 +1,6 @@
 //
 //  SellerMapper.swift
-//  
+//
 //
 //  Created by Elliot Knight on 30/07/2023.
 //
@@ -10,6 +10,13 @@ import CoreLocation
 import Api
 
 extension Seller {
+	/// Initializes a new `Seller` instance using data from a `SellerData` object.
+	///
+	/// - Parameter data: The `SellerData` object containing seller information.
+	/// - Note: This initializer extracts relevant data fields such as name, description,
+	///   and coordinates from the `SellerData` object to create a `Seller` instance.
+	///   It also converts latitude and longitude coordinates to a `CLLocationCoordinate2D`
+	///   object for geospatial information.
 	public init(data: SellerData) {
 		self.name = data.name
 		self.description = data.description
