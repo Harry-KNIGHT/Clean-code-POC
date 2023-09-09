f640698e2f5fff8f914b58f3333addaadc6bd23b
Harry-KNIGHT <eliott.knight@icloud.com>
2023-09-09 16:09:34 +0200

    refac: use BaseApiMock into domain layer for unit tests

---
+++ Domain/Tests/DomainTests/Mocks/BaseApiMock.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Elliot Knight on 09/09/2023.
+//
+
+import Foundation
+import Api
+
+final class BaseApiMock: BaseApi {
+	func sendRequest<T>(url: URL?, responseModel: T.Type) async throws -> T where T : Decodable {
+		guard let url else {
+			throw ServiceError.invalidUrl
+		}
+
+		let data = try Data(contentsOf: url)
+
+		do {
+			let sellers = try JSONDecoder().decode([SellerData].self, from: data)
+			return sellers as! T
+		} catch {
+			throw ServiceError.unknown
+		}
+	}
+}
